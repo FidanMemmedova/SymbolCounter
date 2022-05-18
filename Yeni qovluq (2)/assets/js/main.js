5c86b49a0d76a9744c86e9d596fd4313389dc91a
FidanMemmedova <100687487+FidanMemmedova@users.noreply.github.com>
2022-05-18 15:14:19 +0400

    Add files via upload

---
+++ Yeni qovluq (2)/assets/js/main.js
@@ -0,0 +1,19 @@
+
+function symbolCounter(text) {
+    for (let i = 0; i < text.length; i++) {
+        let count = 0;
+        for (let j = 0; j < text.length; j++) {
+            if (text[i] == text[j] && i > j) {
+                break;
+            }
+            if (text[i] == text[j]) {
+                count++;
+            }
+        }
+        if (count > 0) {
+            console.log(`${text[i]} : ${count}`);
+        }
+    }
+}
+symbolCounter("Kamran")
+
